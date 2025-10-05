e0d5fe64be312c439213b4c311ad51817dc7375e
AndrewMZ6 <Andreika3981@gmail.com>
2025-10-05 23:09:29 +0700

    Routing, slots, props

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <slot></slot>
+    <slot name="kitten"></slot>
+    <em>{{ prop1 }}</em><br>
+    <b>{{ prop2 }}</b><br>
+    <div v-html="prop3">
+    </div>
+    <slot name="doggy"></slot>
+</template>
+
+<script setup>
+    defineProps({
+        prop1: String,
+        prop2: {
+            type: String,
+            default: "I'm prop2. Hello!"
+        },
+        prop3: String,
+    })
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  import MyComponent from '@/components/MyComponent.vue'
+
+  const bodyText = `<h3><b> This is doggy slot </b></h3>`
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <MyComponent prop1="Hello from App.vue" prop2="Hello again from App.vue">
+      <template #kitten>
+        <h1>This is kitten slot</h1>
+      </template>
+      <h2>This is default slot</h2>
+
+      <template #doggy>
+        <div v-html="bodyText"></div>
+      </template>
+    </MyComponent>
+  </div>
 </template>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #c9ced3;
+  background-color: #2c3e50;
+  margin-top: 10px;
+}
+</style>
